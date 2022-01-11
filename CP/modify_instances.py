3836e9cdf45cf05c78a1f4c9deccf6057b5aa8e1
Mattia Bertè <33131887+TiaBerte@users.noreply.github.com>
2022-01-11 18:07:25 +0100

    Add files via upload

---
+++ CP/modify_instances.py
@@ -0,0 +1,29 @@
+# This script read the instance file 'ins-i.txt' and
+# creates a new txt file named 'ins_i.txt' that can be copy in a .dzn file
+# and used as data file in MiniZinc
+# instances_path has must be a folder containing the instances file
+# in the format described by the pdf project.
+
+instances_path = "C:/Users/Mattia/Desktop/cdmo project/instances/"
+txt = [instances_path + "ins-" + str(i)+".txt" for i in range(1, 2)]
+ins_list = ["ins_"+str(i)+".txt" for i in range(1,2)]
+
+for i in range(len(txt)):
+    with open(txt[i]) as data:
+        plaintext = data.readlines()
+
+    s = []
+    for j in range(len(plaintext)):
+        s.append(plaintext[j].replace('\n', ' '))
+
+    f = open(ins_list[i], 'w')
+    f.writelines("W="+s[0]+";\n")
+    f.writelines("n=" + s[1] + ";\n")
+    s[2].replace(" ", ",")
+    f.writelines("dimensions=[|" + s[2].replace(" ", ",") +"\n")
+
+    for j in range(3, len(plaintext)-1):
+        f.writelines("|" + s[j].replace(" ", ",") + "\n")
+
+    f.writelines("|" + s[len(plaintext)-1].replace(" ", ",") +"|]\n")
+    f.close()
---
+++ CP/solution_plot.py
@@ -0,0 +1,84 @@
+# solution_path must contain the path of the solutions.
+# instance must contain the number of the instance whose
+# solution plot is desired.
+
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import random
+
+
+instance = 28
+solution_path = 'C:/Users/Mattia/Desktop/cdmo project/CP/out/out-' \
+                + str(instance) + '.txt'
+try:
+    f = open(solution_path, 'r')
+
+    data = f.read().split('\n')
+    rect = []
+    for i in range(0, len(data)):
+        rect.append(data[i].split())
+    f.close()
+
+    if rect[0][0] == 'No':
+        print('No solution has been found for this instance.')
+
+    else:
+        W = int(rect[0][0])
+        H = int(rect[0][1])
+        n = int(rect[1][0])
+
+        fig, ax = plt.subplots()
+        plt.xticks(range(1,W))
+        plt.yticks(range(1,H))
+        ax.grid()
+        ax.set_xlim(xmin=0, xmax=W)
+        ax.set_ylim(ymin=0, ymax=H)
+
+        for i in range(2, n+2):
+            xi = int(rect[i][2])
+            yi = int(rect[i][3])
+            wi = int(rect[i][0])
+            hi = int(rect[i][1])
+
+            ax.add_patch(Rectangle((xi, yi), wi, hi, fill=1, alpha=1, facecolor=(random.random(), random.random(), random.random())))
+        digits = solution_path[4:]
+        number = digits[:-3]
+        plt.title('Solution of instance {}'.format(instance))
+        plt.show()
+
+except :
+    solution_path = 'C:/Users/Mattia/Desktop/cdmo project/CP/out/out-' \
+                    + str(instance) + '#.txt'
+
+    f = open(solution_path, 'r')
+
+    data = f.read().split('\n')
+    rect = []
+    for i in range(0, len(data)):
+        rect.append(data[i].split())
+    f.close()
+
+    W = int(rect[0][0])
+    H = int(rect[0][1])
+    n = int(rect[1][0])
+
+    fig, ax = plt.subplots()
+    plt.xticks(range(1,W))
+    plt.yticks(range(1,H))
+    ax.grid()
+    ax.set_xlim(xmin=0, xmax=W)
+    ax.set_ylim(ymin=0, ymax=H)
+
+    for i in range(2, n+2):
+        xi = int(rect[i][2])
+        yi = int(rect[i][3])
+        wi = int(rect[i][0])
+        hi = int(rect[i][1])
+        ax.add_patch(Rectangle((xi, yi), wi, hi, fill=1, alpha=1, facecolor=(random.random(), random.random(), random.random())))
+    digits = solution_path[4:]
+    number = digits[:-4]
+
+    print('The solution isn\'t the optimal one.')
+    plt.title('Solution of instance {}'.format(instance))
+    plt.show()
+
